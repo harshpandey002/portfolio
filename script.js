6206e719a38c731c3853c2414edbe22f0ff2a788
pandeyharsh1999 <harsh.pandey@appsecure.security>
2022-07-21 00:41:18 +0530

    Optimizing Animations

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// Scrollbar.init(document.querySelector("#my-scrollbar"));
+const wrapper = document.getElementById("wrapper");
+
 window.addEventListener("load", () => {
   gsap.registerPlugin(ScrollTrigger);
 
@@ -11,8 +12,24 @@ window.addEventListener("load", () => {
       ease: "power1.easeInOut",
       stagger: 0.1,
     })
-    .from(".ui", { x: 130, duration: 1.2, ease: "power1.easeIn" }, "-=0.5")
-    .from(".ux", { x: -130, duration: 1.2, ease: "power1.easeIn" }, "-=1.2")
+    .from(
+      ".ui",
+      {
+        x: window.innerWidth < 800 ? 0 : 130,
+        duration: 1.2,
+        ease: "power1.easeIn",
+      },
+      "-=0.5"
+    )
+    .from(
+      ".ux",
+      {
+        x: window.innerWidth < 800 ? -30 : -130,
+        duration: 1.2,
+        ease: "power1.easeIn",
+      },
+      "-=1.2"
+    )
     .from(
       ".hero-image",
       {
@@ -22,14 +39,42 @@ window.addEventListener("load", () => {
         ease: "power4.easeIn",
       },
       "-=1"
+    )
+    .from(
+      ".slash",
+      {
+        duration: 1.2,
+        scale: 0,
+        ease: "power4.easeIn",
+      },
+      "-=1"
     );
+
+  var imageTl = gsap.timeline();
+
+  imageTl
+    .fromTo(
+      ".hero-image",
+      {
+        scale: 1,
+      },
+      { scale: 4, duration: 1 }
+    )
+    .fromTo(
+      ".hero h1",
+      { scale: 1 },
+      { scale: window.innerWidth < 800 ? 1 : 0.3, duration: 1 },
+      "-=1"
+    );
+
+  ScrollTrigger.create({
+    animation: imageTl,
+    scrub: true,
+  });
 
   var aboutTl = gsap.timeline();
 
   aboutTl
-    .from(".about", {
-      ease: "power4.easeInOut",
-    })
     .from(".about img", { opacity: 0, x: 150 })
     .from(".about p", { opacity: 0, x: -150 });
 
@@ -37,7 +82,7 @@ window.addEventListener("load", () => {
     animation: aboutTl,
     trigger: ".about img",
     start: "top 70%",
-    end: "50% 70%",
+    end: "top 40%",
     scrub: 1,
   });
 
@@ -55,8 +100,17 @@ window.addEventListener("load", () => {
     animation: sectionHeadingTl,
     trigger: ".section-heading span",
     scrub: 0.6,
-    start: "top 100%",
-    end: "top 70%",
+    start: "top 70%",
+    end: "top 40%",
+    // onLeave: () => {
+    //   wrapper.style.background = "#f0f0f0";
+    // },
+    // onEnterBack: () => {
+    //   wrapper.style.background = "#f4f4f4";
+    // },
+    // onLeaveBack: () => {
+    //   wrapper.style.background = "#f4f4f4";
+    // },
   });
 
   var containers = gsap.utils.toArray(".project");
@@ -84,25 +138,30 @@ window.addEventListener("load", () => {
       animation: projectTl,
       trigger: container,
       scrub: 0.6,
-      end: "top 50%",
+      start: "top 60%",
+      end: "top 40%",
     });
   });
 
   var moreTl = gsap.timeline();
 
   moreTl
-    .from(".more", {
-      // width: 0,
-      ease: "power4.easeInOut",
-    })
     .from(".more img", { opacity: 0, x: 150 })
     .from(".more p", { opacity: 0, x: -150 });
 
   ScrollTrigger.create({
     animation: moreTl,
     trigger: ".more",
-    end: "top 50%",
+    start: "top 50%",
+    end: "top 30%",
     scrub: 1,
+    // onEnter: () => {
+    //   wrapper.style.background = "#f4f4f4";
+    // },
+
+    // onLeaveBack: () => {
+    //   wrapper.style.background = "#f0f0f0";
+    // },
   });
 
   var contactHeadingTl = gsap.timeline();
@@ -130,7 +189,8 @@ window.addEventListener("load", () => {
   ScrollTrigger.create({
     animation: contactHeadingTl,
     trigger: ".contact-heading span",
-    end: "top 80%",
+    start: "top 70%",
+    end: "top 40%",
     scrub: 0.6,
   });
 });
